dd3367a5a283113805b05e55c40a9d78530ec80c
Md. Forhad Hossain <forhad14@cse.pstu.ac.bd>
2022-01-27 02:05:16 +0600

    Error solved for chrome driver path

--- Creating-web-scraping-bot/booking/booking.py
+++ Creating-web-scraping-bot/booking/booking.py
@@ -1,19 +1,18 @@
-from selenium.webdriver.chrome.service import Service
-# ser = Service("/Users/forhad/ChromeDriver/chromedriver")
-# op = webdriver.ChromeOptions()
-# driver = webdriver.Chrome(service=ser, options=op)
-
-
 import booking.constants as const
-from selenium import webdriver
 import os
+from selenium import webdriver
 
 
 class Booking(webdriver.Chrome):
-    def __init__(self, driver_path=r"/Users/forhad/ChromeDriver/chromedriver"):
-        Service.driver_path = driver_path
-        os.environ['PATH'] += Service.driver_path
+    def __init__(self, driver_path=r"C:\SeleniumDrivers", teardown=False):
+        self.driver_path = driver_path
+        self.teardown = teardown
+        os.environ['PATH'] += self.driver_path
         super(Booking, self).__init__()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.teardown:
+            self.quit()
 
     def land_first_page(self):
         self.get(const.BASE_URL)
