b7eb3c84149e29bfb6c41d97e54835eec24f2cb8
Md. Forhad Hossain <forhad14@cse.pstu.ac.bd>
2022-02-09 09:14:33 +0600

    With BeautifulSoup

---
+++ Web Scraping with BeautifulSoup4/main.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+print('Put some skill that you are not familiar with')
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill}')
+
+
+def find_job():
+    html_text = requests.get(
+        'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+    for job in jobs:
+        published_date = job.find('span', class_='sim-posted').span.text
+        if 'few' in published_date:
+            company_name = job.find('h3', class_='joblist-comp-name').text.replace('  ', '')
+            skills = job.find('span', class_='srp-skills').text.replace('  ', '')
+            more_info = job.header.h2.a['href']
+            if unfamiliar_skill not in skills:
+                print(f'Company Name: {company_name.strip()}')
+                print(f'Skills: {skills.strip()}')
+                print(f'More info: {more_info.strip()}')
+                print(' ')
+
+
+if __name__ == '__main__':
+    while True:
+        find_job()
+        time_wait = 10
+        print(f'Waiting {time_wait} minutes')
+        time.sleep(time_wait*60)
